48a2d5f184a34b6e8dd4230ce3294e110dc1a6ff
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2026-08-01 17:00:10 +0200

    5.2

--- kubejs/client_scripts/ItemTooltips.js
+++ kubejs/client_scripts/ItemTooltips.js
@@ -1,3 +1,8 @@
 ItemEvents.modifyTooltips(event => {
+
     event.add('pipez:improved_upgrade', Text.red('This item\'s recipe has been removed, craft the advanced upgrade instead.'))
+
+    event.modify('oritech:fluxite', tooltip => {
+        tooltip.removeLine(1);
+    });
 })
